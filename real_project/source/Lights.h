414654eb5e0648c5f31db25675d4beb8c8e2f7fd
Peder Aa. Hoff <peder.aa.hoff@gmail.com>
2026-02-26 14:43:07 +0100

    La til noe flere funkjsoner, ob begynte på update_lights-funksjonen

--- real_project/source/Lights.h
+++ real_project/source/Lights.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <elevio.h>
+
 typedef struct {
-    int floor_lights[4];
-    int panel_lights[4];
-    int opp_lights[4];
-    int ned_lights[4];
+    int floor_lights[N_FLOORS];
+    int panel_lights[N_FLOORS];
+    int opp_lights[N_FLOORS];
+    int ned_lights[N_FLOORS];
 } Lights;
 
+Lights lights_initilize();
 void lights_setfloorIndicator(Lights *lights, int floor, int value);
 void lights_setOrderlIght(Lights *lights, int floor, MotorDirection dir, int value);
+void lights_updateLights(Lights *lights);
