fa20e53f73137813b4c584f4d901daa7847cd5f6
Arda Dogan <ardahd@stud.ntnu.no>
2026-02-26 14:41:47 +0100

    added choose dir function and fixed functions and includes.

--- real_project/source/Elevator_controller.c
+++ real_project/source/Elevator_controller.c
@@ -1,12 +1,23 @@
 #include "Elevator_controller.h"
+#include "elevio.h"
+#include "Elevator.h"
+#include <stdbool.h>
 
 
 bool elevator_controller_should_stop(int floor, Elevator_controller ec){
     return (floor == ec.target_floor);
 };
 
-void elevator_controller_new_target_floor(int target, Elevator_controller* ec){
+void elevator_controller_new_target_floor(Elevator_controller* ec, int target){
     ec->target_floor = target;
 };
 
-//choose_dir? 
+MotorDirection elevator_controller_choose_dir(Elevator_controller* ec, Elevator* e){
+    if (ec->target_floor > e->current_floor){
+        e->direction = DIRN_UP;
+    } else if (ec->target_floor < e->current_floor){
+        e->direction = DIRN_DOWN;
+    } else {
+        e->direction = DIRN_STOP;
+    }
+} 
