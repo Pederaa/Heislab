db92d6de27e087d365b8bb052973d9a7ab3b93c4
Arda Dogan <ardahd@stud.ntnu.no>
2026-03-05 12:29:54 +0100

    Fikset feil i elevator controller

--- real_project/source/Elevator_controller.c
+++ real_project/source/Elevator_controller.c
@@ -12,12 +12,12 @@ void elevator_controller_new_target_floor(Elevator_controller* ec, int target){
     ec->target_floor = target;
 };
 
-void elevator_controller_choose_dir(Elevator_controller* ec, Elevator* e){
+MotorDirection elevator_controller_choose_dir(Elevator_controller* ec, Elevator* e){
     if(e->current_floor == 0){
         e->direction = DIRN_UP;
     }
     else if(e->current_floor == N_FLOORS-1){
-        e->.direction = DIRN_DOWN;
+        e->direction = DIRN_DOWN;
     }
     
     else if (ec->target_floor > e->current_floor){
