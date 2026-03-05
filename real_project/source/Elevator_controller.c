ac97b034b227bd5389fa75bab18e14aab9cc5083
Markus Andresen <markujan@stud.ntnu.no>
2026-03-05 15:08:56 +0100

    Begynte på en elevator_controller

--- real_project/source/Elevator_controller.c
+++ real_project/source/Elevator_controller.c
@@ -1,6 +1,7 @@
 #include "Elevator_controller.h"
 #include "driver/elevio.h"
 #include "Elevator.h"
+#include "Lights.h"
 #include <stdbool.h>
 
 
@@ -12,19 +13,50 @@ bool elevator_controller_should_stop(int floor, bool at_floor, Elevator_controll
     return (floor == ec.target_floor);
 };
 
-void elevator_controller_new_target_floor(Elevator_controller* ec, int target){
-    ec->target_floor = target;
-};
 
-void elevator_controller_choose_dir(Elevator_controller* ec, Elevator* e){
+void elevator_controller_update_target_floor(Elevator_controller* ec, Elevator* e, Lights* l){
+    MotorDirection current_dir = e->direction;
+
+    if (current_dir == DIRN_UP){
+        for (int floor=e->current_floor; floor<N_FLOORS; floor++){
+            if (l->panel_lights[floor] == 1){
+                return floor;
+            }
+        }
+
+        for (int floor=e->current_floor; floor>=0; floor--){
+            if (l->panel_lights[floor] == 1){
+                return floor;
+            }
+        }
+    }
+
+    else if (current_dir == DIRN_DOWN){
+        for (int floor=e->current_floor; floor>=0; floor--){
+            if (l->panel_lights[floor] == 1){
+                return floor;
+            }
+        }
+        
+        for (int floor=e->current_floor; floor<N_FLOORS; floor++){
+            if (l->panel_lights[floor] == 1){
+                return floor;
+            }
+        }
+    }
+}
+
+void elevator_controller_choose_dir(Elevator_controller* ec, Elevator* e, Lights* l){
     if(e->current_floor == 0){
         e->direction = DIRN_UP;
     }
     else if(e->current_floor == N_FLOORS-1){
         e->direction = DIRN_DOWN;
     }
+
+    elevator_controller_update_target_floor(ec, e, l);
     
-    else if (ec->target_floor > e->current_floor){
+    if (ec->target_floor > e->current_floor){
         e->direction = DIRN_UP;
     } 
     else if (ec->target_floor < e->current_floor){
